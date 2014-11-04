5064b9ebbb820605b0d0205111544ddb4a672460
aprel_co <sasha.g@w-not.ru>
2014-11-04 19:26:26 +0300

    captcha color fix

--- app/config/packages/sayakb/captcha/config.php
+++ app/config/packages/sayakb/captcha/config.php
@@ -9,7 +9,7 @@ return array(
     'height' => 30,
     'space' => 20,
     'type' => 'alnum',
-    'colors' => array('255,0,0', '188,44,55', '0,0,0', '40,40,40', '188,188,188'),
+    'colors' => array('255,0,0', '188,44,55', '0,0,0', '40,40,40'),
     'sensitive' => FALSE
 
 );
