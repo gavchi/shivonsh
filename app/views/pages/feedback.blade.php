00dc00692deefbbdb600578298913a843ac83476
aprel_co <sasha.g@w-not.ru>
2014-11-13 22:06:24 +0300

    feedback

--- app/views/pages/feedback.blade.php
+++ app/views/pages/feedback.blade.php
@@ -4,11 +4,11 @@
 <h3 class="active">​SHIVONSH | Alexandra Sh</h3>
 <h4>shivonsh@gmail.com</h4>
 <ul class="social">
-    <li><a target="_blank" href="https://www.facebook.com/Shivonsh" class="fb"></a></li>
+    {{--<li><a target="_blank" href="https://www.facebook.com/Shivonsh" class="fb"></a></li>--}}
     <li><a target="_blank" href="http://vk.com/shivonsh" class="vk"></a></li>
     <li><a target="_blank" href="http://instagram.com/shivonsh" class="in"></a></li>
     <li><a target="_blank" href="http://shivonsh.tumblr.com/" class="tb"></a></li>
-    <li><a target="_blank" href="https://twitter.com/shivonsh" class="tw"></a></li>
+    {{--<li><a target="_blank" href="https://twitter.com/shivonsh" class="tw"></a></li>--}}
 </ul>
 <div class="form">
 @if(isset($messages))
