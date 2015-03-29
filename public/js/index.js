e51b94a9828abf794cfe16e3fc4017f971111104
aprel_co <sasha.g@w-not.ru>
2015-03-29 15:00:28 +0300

    redesign

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,7 @@ function startFancy(selector){
                 location.hash = links.href.substring(8);
                 this.title = '<ul class="social_i">' +
                 '<li><a href="http://www.facebook.com/sharer.php?s=100&p[url]='+location.href+'&p[images][0]=http://'+location.host+'/i/'+$(this.element).attr('href')+'" class="fb" target="_blank"></a></li>' +
-                '<li><a href="http://www.tumblr.com/share?url='+location.href+'" class="tb" target="_blank"></a></li>' +
+                '<li><a href="http://www.tumblr.com/share?url=http://'+location.host+'/i/'+$(this.element).attr('href')+'" class="tb" target="_blank"></a></li>' +
                 '<li><a href="http://twitter.com/share?url='+location.href+'&related=shivonsh" class="tw" target="_blank"></a></li>' +
                 '<li><a href="http://vkontakte.ru/share.php?url='+location.href+'&image=http://'+location.host+'/i/'+$(this.element).attr('href')+'" class="vk" target="_blank"></a></li>' +
                 '</ul>';
